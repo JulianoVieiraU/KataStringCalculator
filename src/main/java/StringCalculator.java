dae0da00676c2b12d2c133942e275d92821925ec
Juliano <juliano.vieira@ubots.com.br>
2021-07-11 20:12:07 -0300

    11- Refactor

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,21 +2,29 @@ public class StringCalculator {
 
     public String add(String values){
         if(!values.isEmpty()) {
-            if(values.contains(",")) {
-                String[] valuesSplited = values.split(",|\n");
-                double sum = 0.0;
-                for (String value : valuesSplited) {
-                    sum += Double.parseDouble(value);
-                }
-
-                if(sum %((int)sum) == 0) return Integer.toString((int)sum);
-
-                return Double.toString(Math.round(sum * 100.0) / 100.0);
-            }
-
-            return values;
+            return sum(values);
         }
 
         return "0";
+    }
+
+    private String sum(String values){
+        if(containsSeparator(values)) {
+            String[] valuesSplited = values.split(",|\n");
+            double sum = 0.0;
+            for (String value : valuesSplited) {
+                sum += Double.parseDouble(value);
+            }
+
+            if(sum %((int)sum) == 0) return Integer.toString((int)sum);
+
+            return Double.toString(Math.round(sum * 100.0) / 100.0);
+        }
+
+        return values;
+    }
+
+    private boolean containsSeparator(String values){
+        return values.contains(",") || values.contains("\n");
     }
 }
